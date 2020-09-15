c61f725863c65c74762d1bc2b6bbf303dd892ddc
Mohibul Hassan Chowdhury <mohibul.chowdhury@welldev.io>
2020-09-15 10:59:20 +0600

    add ViewResolver in java configuration file

--- src/main/java/testwebmvc/MVCConfig.java
+++ src/main/java/testwebmvc/MVCConfig.java
@@ -1,11 +1,23 @@
 package testwebmvc;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan({"testwebmvc"})
 public class MVCConfig implements WebMvcConfigurer {
 
+    @Bean
+    public ViewResolver viewResolver() {
+
+        InternalResourceViewResolver vr = new InternalResourceViewResolver();
+        vr.setPrefix("/WEB-INF/view/");
+        vr.setSuffix(".jsp");
+
+        return vr;
+    }
 }
